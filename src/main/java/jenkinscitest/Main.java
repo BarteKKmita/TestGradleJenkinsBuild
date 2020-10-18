480732dc3e2edc907c664022a2d2aedf7d398c6d
BarteKKmita <bartekkmitaa@gmail.com>
2020-10-18 18:36:38 +0200

    add test feature

--- src/main/java/jenkinscitest/Main.java
+++ src/main/java/jenkinscitest/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Trying to implement Jenkins Pull Request checks on github with Jenkins on EC2");
         Map<Integer,Integer> test= new HashMap<>();
+        Map<Integer,Integer> tes1t= new HashMap<>();
     }
 }
